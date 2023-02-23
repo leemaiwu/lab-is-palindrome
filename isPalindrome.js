7e896dc7b552db33f75babf1cd02bcc66b679102
Laura Wu <leemaiwu@gmail.com>
2023-02-23 10:42:00 -0700

    Updated code to account for longer words

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,11 +1,14 @@
 // Write your code below
 
-// create a function that takes in a parameter for the "word"
-// parameter will be the "word" we want to test as a palindrome
-// second parameter will be to loop through the word
-// then check the reverse to see it it's true
-// else return flase
+// create a function that takes in a parameter for the string
+// create a for loop that goes through the length of the string
+// then another for loop that goes through the reverse of the word
+// if the word and the index of the first loop equals
+// the word and the index of the scond loop
+// return true else false
 
+
+// This returns true too soon and doesn't loop through the entire word
 function palindrome(str) {
   for (let i = 0; i < str.length; i++) {
     for (let k = str.length-1; k > 0; k--) {
@@ -18,7 +21,27 @@ function palindrome(str) {
   } 
 }
 
-console.log(palindrome("noon"))
+console.log(palindrome("nodson"))
 
-const word = palindrome("noon")
+const word = palindrome("Non")
 console.log(word)
+
+
+
+// This code fixes that issue
+function isPal(str) {
+  for (let i = 0; i < str.length; i++) {
+      let charF = str[i]
+      let charB = str[str.length - 1 - i]
+
+      if (charF !== charB) {
+          return false
+      }
+  }
+  return true
+}
+
+console.log(isPal("nodson"))
+
+const word2 = isPal("Non")
+console.log(word2)
